99fc6d1f779e1e14f25da7bacf4ef571b3b791ed
Felix Colaci <felix.colaci@okta.com>
2022-04-04 11:02:18 +0200

    feat: include function for deep merging the user profile

--- index.js
+++ index.js
@@ -67,3 +67,32 @@ exports.calculateSchemaToRequest = (schema, user_metadata, multipage = true, max
 }
 
 exports.progressiveProfilingNeeded = (schema) => schema?.properties && Object.keys(schema?.properties).length > 0;
+
+/**
+ * Performs a deep merge of `source` into `target`.
+ * Mutates `target` only but not its objects and arrays.
+ *
+ * @author inspired by [jhildenbiddle](https://stackoverflow.com/a/48218209).
+ */
+ exports.mergeAttributesToProfile = (target, source) => {
+    const isObject = (obj) => obj && typeof obj === 'object';
+  
+    if (!isObject(target) || !isObject(source)) {
+      return source;
+    }
+  
+    Object.keys(source).forEach(key => {
+      const targetValue = target[key];
+      const sourceValue = source[key];
+  
+      if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+        target[key] = targetValue.concat(sourceValue);
+      } else if (isObject(targetValue) && isObject(sourceValue)) {
+        target[key] = mergeDeep(Object.assign({}, targetValue), sourceValue);
+      } else {
+        target[key] = sourceValue;
+      }
+    });
+  
+    return target;
+  }
